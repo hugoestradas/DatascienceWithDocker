e1600dd54e374e8ec88fd208d49faba0a924bfdc
Hugo Estrada <61108175+hugoestradas@users.noreply.github.com>
2020-04-20 17:47:01 -0600

    Update az.sh

--- prerequisites/az.sh
+++ prerequisites/az.sh
@@ -2,16 +2,16 @@ az login
 
 az vm list-sizes -l "eastus"
 
-az group create -name "datasciencerg" -l "eastus"
+az group create -name "hergdatascience" -l "eastus"
 
-az vm create --resource-group "datasciencerg" -n "datasciencevm" --image "UbuntuLTS"  --size "Standard_B2ms"  --admin-username "datascience" --admin-password "Datascience@123" -l "eastus"
+az vm create --resource-group "hergdatascience" -n "vm" --image "UbuntuLTS"  --size "Standard_B2ms"  --admin-username "vmuser" --admin-password "P@ssword12345" -l "eastus"
 
-az vm open-port  --port 22   --resource-group "datasciencerg" -n "datasciencevm" --priority 100
-az vm open-port  --port 80   --resource-group "datasciencerg" -n "datasciencevm" --priority 101
-az vm open-port  --port 8888 --resource-group "datasciencerg" -n "datasciencevm" --priority 102
-az vm open-port  --port 5432 --resource-group "datasciencerg" -n "datasciencevm" --priority 103
-az vm open-port  --port 8081 --resource-group "datasciencerg" -n "datasciencevm" --priority 104
-az vm open-port  --port 8088 --resource-group "datasciencerg" -n "datasciencevm" --priority 105
-az vm open-port  --port 9000 --resource-group "datasciencerg" -n "datasciencevm" --priority 106
+az vm open-port  --port 22   --resource-group "hergdatascience" -n "vm" --priority 100
+az vm open-port  --port 80   --resource-group "hergdatascience" -n "vm" --priority 101
+az vm open-port  --port 8888 --resource-group "hergdatascience" -n "vm" --priority 102
+az vm open-port  --port 5432 --resource-group "hergdatascience" -n "vm" --priority 103
+az vm open-port  --port 8081 --resource-group "hergdatascience" -n "vm" --priority 104
+az vm open-port  --port 8088 --resource-group "hergdatascience" -n "vm" --priority 105
+az vm open-port  --port 9000 --resource-group "hergdatasciencex" -n "vm" --priority 106
 
 #az group delete -n "datasciencerg"
