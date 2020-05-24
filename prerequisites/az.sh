65994484f06d56b2d785b0eb366819b382efe67b
Hugo Estrada <61108175+hugoestradas@users.noreply.github.com>
2020-05-24 02:04:55 -0600

    Update az.sh

--- prerequisites/az.sh
+++ prerequisites/az.sh
@@ -2,16 +2,16 @@ az login
 
 az vm list-sizes -l "eastus"
 
-az group create --name "hergdatascience" -l "eastus"
+az group create --name "hugoestradasdatasciencerg" -l "eastus"
 
-az vm create --resource-group "hergdatascience" -n "vm" --image "UbuntuLTS"  --size "Standard_B2ms"  --admin-username "vmuser" --admin-password "P@ssword12345" -l "eastus"
+az vm create --resource-group "hugoestradasdatasciencerg" -n "vm-dsenv" --image "UbuntuLTS"  --size "Standard_B2ms"  --admin-username "vm-dsenv" --admin-password "P@ssword12345" -l "eastus"
 
-az vm open-port  --port 22   --resource-group "hergdatascience" -n "vm" --priority 100
-az vm open-port  --port 80   --resource-group "hergdatascience" -n "vm" --priority 101
-az vm open-port  --port 8888 --resource-group "hergdatascience" -n "vm" --priority 102
-az vm open-port  --port 5432 --resource-group "hergdatascience" -n "vm" --priority 103
-az vm open-port  --port 8081 --resource-group "hergdatascience" -n "vm" --priority 104
-az vm open-port  --port 8088 --resource-group "hergdatascience" -n "vm" --priority 105
-az vm open-port  --port 9000 --resource-group "hergdatascience" -n "vm" --priority 106
+az vm open-port  --port 22   --resource-group "hugoestradasdatasciencerg" -n "vm-dsenv" --priority 100
+az vm open-port  --port 80   --resource-group "hugoestradasdatasciencerg" -n "vm-dsenv" --priority 101
+az vm open-port  --port 8888 --resource-group "hugoestradasdatasciencerg" -n "vm-dsenv" --priority 102
+az vm open-port  --port 5432 --resource-group "hugoestradasdatasciencerg" -n "vm-dsenv" --priority 103
+az vm open-port  --port 8081 --resource-group "hugoestradasdatasciencerg" -n "vm-dsenv" --priority 104
+az vm open-port  --port 8088 --resource-group "hugoestradasdatasciencerg" -n "vm-dsenv" --priority 105
+az vm open-port  --port 9000 --resource-group "hugoestradasdatasciencerg" -n "vm-dsenv" --priority 106
 
 #az group delete -n "hergdatascience"
